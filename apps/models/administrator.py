4e25964ac2e4aee01ef731e7844aef7e7c25c54c
Hiroto Fukuyama <bb38124082@ms.nagasaki-u.ac.jp>
2026-06-26 14:13:00 +0900

    Adimistratorにパスワードチェック機能追加

--- apps/models/administrator.py
+++ apps/models/administrator.py
@@ -1,5 +1,15 @@
+import hashlib
 class Administrator:
-    def __init__(self, admin_id, admin_name,password):
+    def __init__(self, admin_id, admin_name, password):
         self.admin_id = admin_id
         self.admin_name = admin_name
-        self.password = password
+        self.password_hash = self._hash_password(password)
+
+    def _hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+    
+    def check_password(self, password):
+        return self.password_hash == self._hash_password(password)
+    
+    def __str__(self):
+        return f"Admin({self.admin_id}, {self.admin_name})"
