166008c8a6cf7ca0e8fdb17d69aeae5493ef3d26
Hiroto Fukuyama <bb38124082@ms.nagasaki-u.ac.jp>
2026-06-26 14:02:49 +0900

    administratorの 一部作成

---
+++ apps/models/administrator.py
@@ -0,0 +1,5 @@
+class Administrator:
+    def __init__(self, admin_id, admin_name,password):
+        self.admin_id = admin_id
+        self.admin_name = admin_name
+        self.password = password
