cc8e6af83bfe890d6230ba82c98eab1652b2ad67
Hiroto Fukuyama <bb38124082@ms.nagasaki-u.ac.jp>
2026-07-03 13:30:28 +0900

    受取り処理の追加

--- apps/app/controllers/product_controller.py
+++ apps/app/controllers/product_controller.py
@@ -8,8 +8,9 @@ from apps.app.models.Product import Product
 class ProductController:
     PRODUCT_JSON_PATH = os.path.join(os.path.dirname(__file__), "../data/Product.json")
     DELETED_JSON_PATH = os.path.join(os.path.dirname(__file__), "../data/Deleted_product.json")
-    
 
+
+    #=================================登録=========================================
     def validate_product(self, product_id, product_name, customer_name, delivery_date, deadline, driver_id):
         if not product_id or not product_name or not customer_name or not delivery_date or not deadline or not driver_id:
             print("error: 全ての項目を入力してください。")
@@ -33,18 +34,22 @@ class ProductController:
 
         return True
     
+    #===============================期限チェック=========================================
     def check_deadline(self,deadline):
         if datetime.date.today() > deadline:
             print("error: 受け取り期限が過ぎています。")
             return False
         return True
-
+    
+    #==============================商品検索=========================================
     def search_items(self,products,product_id,customer_name):
         for product in products:
             if product.product_id == product_id and product.customer_name == customer_name:
                 return product
         return None
+    
 
+    #==============================削除処理=========================================
     def confirm_delete(self, products: List[Product], target_product: Product) -> bool:
         """
         商品の削除確認と削除実行を行います。
@@ -57,3 +62,11 @@ class ProductController:
         else:
             print("error: 削除対象の商品が見つかりません。")
             return False
+        
+    #==============================受取処理=========================================
+    def receive_product(self, product: Product) -> None:
+        """
+        商品の受取処理を行います。
+        """
+        product.status = "受取り済み"
+        print(f"success: 商品名「{product.product_name}」の受取が完了しました。")
